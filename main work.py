6ee7aa1fca291fbfd9a62c90482c5d218ea08ae0
zimaster <zimaster@localhost.localdomain>
2022-07-15 06:30:55 +0300

    complete project and documentation

---
+++ main work.py
@@ -0,0 +1,66 @@
+import cv2
+from pickle import load
+from datetime import datetime
+from numpy import argmin
+from face_recognition import face_locations,face_encodings,face_distance
+
+
+encodin_list = load( open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/encodin_list.p", "r+b" ) )
+class_name = load( open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/class_name.p", "r+b" ) )
+
+def attendence(name):
+    with open('/home/zimaster/Desktop/Employee-attendance-by-face-recognition/Today attendance.csv','r+') as f:
+        myData = f.readlines()
+        namelist = []
+        for line in myData:
+            entry = line.split(',')[0]
+            namelist.append(entry)
+        if name not in namelist:
+            f.writelines(f"\n{name},{datetime.now()}")
+
+def detact_human(img_s):
+    img_s = cv2.resize(img, (0,0), None, 0.33, 0.33)
+
+    img_s = cv2.cvtColor(img_s, cv2.COLOR_BGR2RGB)
+    faceLoc_,name_ = [],[]
+
+    facesCurFrame = face_locations(img_s)
+    encodeCurFrame = face_encodings(img_s, facesCurFrame)
+
+    for i in range(5):
+        for encodeFace,faceLoc in zip(encodeCurFrame,facesCurFrame):
+
+            faceDis = face_distance(encodin_list , encodeFace)
+
+            find = argmin(faceDis)
+            name = class_name[find].upper()
+
+            if str(name in name_) == 'False' and faceDis[find]*100 < 48.00:
+                faceLoc_.append(faceLoc)
+                name_.append(name)
+    return faceLoc_,name_
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+
+    _,img = cap.read()
+
+    faceLoc_,name_ = detact_human(img)
+
+    for faceLoc,name in zip(faceLoc_,name_):
+
+        y1,x2,y2,x1 = faceLoc
+
+        y1,x2,y2,x1 = y1*3, x2*3, y2*3, x1*3
+
+        img = cv2.rectangle(img, (x1,y1), (x2,y2), (36,255,12), 1)
+
+        cv2.putText(img, name, (x1, y1-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 166, 255), 1)
+
+        attendence(name)
+
+    cv2.imshow("Cam_1", img)
+    if cv2.waitKey(1) == ord('q'): break
+
+cv2.destroyAllWindows()()
---
+++ train_model.py
@@ -0,0 +1,47 @@
+import cv2,os
+from pickle import load,dump
+import face_recognition
+
+encodin_list = load( open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/encodin_list.p", "r+b" ) )
+class_name = load( open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/class_name.p", "r+b" ) )
+
+user_inp = int(input("1. Add one picture and Training\n2. Add more picture and Training\n3. Clear all previous record data\nChoose One : "))
+
+def add_data(img_path):
+
+    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+
+    try:encodin_list.append( face_recognition.face_encodings(img)[0] )
+    except:print(f"Face not Detacted. remove {img_path.split('/')[-1]}")
+    
+    nam = img_path.split('/')[-1]
+    class_name.append(nam.split()[0])
+    
+    print(F"Done {nam}")
+    
+
+if user_inp == 1:
+    
+    dic_tory = input("The location of the picture you want to add : ")
+    add_data(dic_tory)
+
+    dump( encodin_list, open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/encodin_list.p", "wb" ) )
+    dump( class_name, open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/class_name.p", "wb" ) )
+
+
+elif user_inp == 2:
+    
+    dic_tory = input("The folder location of the images you want to add: ")
+
+    for i in os.listdir(dic_tory):
+        
+        add_data(f"{dic_tory}/{i}")
+
+    dump( encodin_list, open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/encodin_list.p", "wb" ) )
+    dump( class_name, open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/class_name.p", "wb" ) )
+
+elif user_inp == 3:
+    dump( [], open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/encodin_list.p", "wb" ) )
+    dump( [], open( "/home/zimaster/Desktop/Employee-attendance-by-face-recognition/class_name.p", "wb" ) )
+
+print("\nDone........")
